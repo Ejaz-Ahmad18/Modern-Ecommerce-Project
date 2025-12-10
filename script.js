9cbfd270e8c848a5fe4e0c396f10ae8fd9e8d864
ejazahmad18 <numen.ejaz@gmail.com>
2025-12-10 18:30:00 +0530

    fixing-js-css code

--- script.js
+++ script.js
@@ -1,45 +1,47 @@
 // AOS Animation
 
-AOS.init(duration=1000,once=true);
+AOS.init({ 
+    duration: 1000,
+     once: true ,
+    });
 
 // mobile menu
 
-const hamburger=document.getElementById("hamburgerBtn")
-const menu=document.querySelector(".menu")
+const hamburger = document.getElementById("hamburgerBtn");
+const menu = document.querySelector(".menu");
 
-hamburger.addEventListener('click',()=>{
-    menu.classlist.toggle('active'); 
+hamburger.addEventListener("click", () => {
+  menu.classList.toggle("active");
 });
 
 //dropdown logic
 
-const dropdowns=document.querySelectorAll('.dropdown');
+const dropdowns = document.querySelectorAll(".dropdown");
 
-dropdowns.forEach(dropdown=>{
-    dropdown.querySelector('.dropdown-btn').addEventListener('click',(e)=>{
-e.stopPropagation();
-dropdowns.forEach(d=>d.classList.remove('active'));
-dropdown.classList.toggle("active");
-    })
-})
+dropdowns.forEach((dropdown) => {
+  dropdown.querySelector(".dropdown-btn").addEventListener("click", (e) => {
+    e.stopPropagation();
+    dropdowns.forEach((d) => d.classList.remove("active"));
+    dropdown.classList.toggle("active");
+  });
+});
 
-document.activeElement('click',()=>{
-dropdowns.forEach(d=>d.classList.remove('active'));
-})
+document.addEventListener("click", () => {
+  dropdowns.forEach((d) => d.classList.remove("active"));
+});
 
 // search button animation
 
-const searchBtn=document.getElementById('searchBtn');
-const searchBox=document.getElementById('searchBox');
-const closeSearch=document.getElementById('closeSearch');
+const searchBtn = document.getElementById("searchBtn");
+const searchBox = document.getElementById("searchBox");
+const closeSearch = document.getElementById("closeSearch");
 
-searchBtn.onclick=()=>{
-    searchBox.classList.add('active');
-    searchBtn.classList.add('hide');
+searchBtn.onclick = () => {
+  searchBox.classList.add("active");
+  searchBtn.classList.add("hide");
+};
 
-}
-
-closeSearch.onclick=()=>{
-    searchBox.classList.remove('active');
-    searchBtn.classList.remove('hide')
-}
+closeSearch.onclick = () => {
+  searchBox.classList.remove("active");
+  searchBtn.classList.remove("hide");
+};
